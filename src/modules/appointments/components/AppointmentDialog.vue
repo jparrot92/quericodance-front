5b64049f9427d5b978d8908bcd4950672332ab9c
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-07-28 01:25:34 +0200

    Dialogo eventos

---
+++ src/modules/appointments/components/AppointmentDialog.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+interface Props {
+    isOpen: boolean;
+}
+interface Emits {
+    (e: 'onClose'): void;
+}
+const props = defineProps<Props>();
+const emits = defineEmits<Emits>();
+
+const isOpen = ref<boolean>(false);
+
+watch(props, () => {
+    isOpen.value = props.isOpen;
+});
+</script>
+
+<template>
+    <q-dialog v-model="isOpen" @hide="emits('onClose')">
+        <q-card>
+            <q-toolbar>
+                <q-toolbar-title> Framework </q-toolbar-title>
+
+                <q-btn
+                    flat
+                    round
+                    dense
+                    icon="close"
+                    @click="emits('onClose')"
+                    v-close-popup
+                />
+            </q-toolbar>
+
+            <q-separator />
+
+            <q-card-section style="max-height: 50vh" class="scroll">
+                <p v-for="n in 15" :key="n">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Rerum repellendus sit voluptate voluptas eveniet porro.
+                    Rerum blanditiis perferendis totam, ea at omnis vel numquam
+                    exercitationem aut, natus minima, porro labore.
+                </p>
+            </q-card-section>
+
+            <q-separator />
+
+            <q-card-actions align="right">
+                <q-btn
+                    @click="emits('onClose')"
+                    flat
+                    label="Decline"
+                    color="primary"
+                    v-close-popup
+                />
+                <q-btn flat label="Accept" color="primary" v-close-popup />
+            </q-card-actions>
+        </q-card>
+    </q-dialog>
+</template>
--- src/modules/appointments/components/WeekCalendar.vue
+++ src/modules/appointments/components/WeekCalendar.vue
@@ -10,6 +10,10 @@
                     view="week"
                     locale="es"
                     :weekdays="[1, 2, 3, 4, 5, 6, 0]"
+                    :interval-minutes="15"
+                    :interval-count="96"
+                    :hour24-format="true"
+                    :hoverable="true"
                     no-active-date
                     animated
                     bordered
@@ -22,33 +26,324 @@
                     @click-interval="onClickInterval"
                     @click-head-intervals="onClickHeadIntervals"
                     @click-head-day="onClickHeadDay"
-                />
+                >
+                    <template #head-day-event="{ scope: { timestamp } }">
+                        <div
+                            style="
+                                display: flex;
+                                justify-content: center;
+                                flex-wrap: wrap;
+                                padding: 2px;
+                            "
+                        >
+                            <template
+                                v-for="event in eventsMap[timestamp.date]"
+                                :key="event.id"
+                            >
+                                <q-badge
+                                    v-if="!event.time"
+                                    :class="badgeClasses(event, 'header')"
+                                    :style="badgeStyles(event, 'header')"
+                                    style="
+                                        width: 100%;
+                                        cursor: pointer;
+                                        height: 12px;
+                                        font-size: 10px;
+                                        margin: 1px;
+                                    "
+                                >
+                                    <span class="title q-calendar__ellipsis">
+                                        {{ event.title }}
+                                        <q-tooltip>{{
+                                            event.details
+                                        }}</q-tooltip>
+                                    </span>
+                                </q-badge>
+                                <q-badge
+                                    v-else
+                                    :class="badgeClasses(event, 'header')"
+                                    :style="badgeStyles(event, 'header')"
+                                    style="
+                                        margin: 1px;
+                                        width: 10px;
+                                        max-width: 10px;
+                                        height: 10px;
+                                        max-height: 10px;
+                                    "
+                                    @click="scrollToEvent(event)"
+                                >
+                                    <q-tooltip>{{
+                                        event.time + ' - ' + event.details
+                                    }}</q-tooltip>
+                                </q-badge>
+                            </template>
+                        </div>
+                    </template>
+
+                    <template
+                        #day-body="{
+                            scope: {
+                                timestamp,
+                                timeStartPos,
+                                timeDurationHeight
+                            }
+                        }"
+                    >
+                        <template
+                            v-for="event in getEvents(timestamp.date)"
+                            :key="event.id"
+                        >
+                            <div
+                                v-if="event.time !== undefined"
+                                class="my-event"
+                                :class="badgeClasses(event, 'body')"
+                                :style="
+                                    badgeStyles(
+                                        event,
+                                        'body',
+                                        timeStartPos,
+                                        timeDurationHeight
+                                    )
+                                "
+                            >
+                                <span class="title q-calendar__ellipsis">
+                                    {{ event.title }}
+                                    <q-tooltip>{{ event.details }}</q-tooltip>
+                                </span>
+                            </div>
+                        </template>
+                    </template>
+                </q-calendar-day>
             </div>
         </div>
     </div>
+    <AppointmentDialog :is-open="isOpen" @on-close="isOpen = false" />
 </template>
 
 <script>
-import { QCalendarDay, today } from '@quasar/quasar-ui-qcalendar/src/index.js';
+import {
+    QCalendarDay,
+    addToDate,
+    parseTimestamp,
+    isBetweenDates,
+    today,
+    parsed,
+    parseDate,
+    parseTime
+} from '@quasar/quasar-ui-qcalendar/src/QCalendarDay.js';
 import '@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass';
 import '@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass';
 import '@quasar/quasar-ui-qcalendar/src/QCalendarDay.sass';
 
 import { defineComponent } from 'vue';
-import NavigationBar from './NavigationBar.vue';
+import NavigationBar from '../components/NavigationBar.vue';
+import AppointmentDialog from '../components/AppointmentDialog.vue';
+
+// The function below is used to set up our demo data
+const CURRENT_DAY = new Date();
+function getCurrentDay(day) {
+    const newDay = new Date(CURRENT_DAY);
+    newDay.setDate(day);
+    const tm = parseDate(newDay);
+    return tm.date;
+}
 
 export default defineComponent({
-    name: 'WeekNoActiveDate',
+    name: 'WeekSlotDayBody',
     components: {
         NavigationBar,
-        QCalendarDay
+        QCalendarDay,
+        AppointmentDialog
     },
     data() {
         return {
-            selectedDate: today()
+            selectedDate: today(),
+            events: [
+                {
+                    id: 1,
+                    title: '1st of the Month',
+                    details:
+                        'Everything is funny as long as it is happening to someone else',
+                    date: getCurrentDay(1),
+                    bgcolor: 'orange'
+                },
+                {
+                    id: 2,
+                    title: 'Sisters Birthday',
+                    details: 'Buy a nice present',
+                    date: getCurrentDay(4),
+                    bgcolor: 'green',
+                    icon: 'fas fa-birthday-cake'
+                },
+                {
+                    id: 3,
+                    title: 'Meeting',
+                    details: 'Time to pitch my idea to the company',
+                    date: getCurrentDay(10),
+                    time: '10:00',
+                    duration: 120,
+                    bgcolor: 'red',
+                    icon: 'fas fa-handshake'
+                },
+                {
+                    id: 4,
+                    title: 'Lunch',
+                    details: 'Company is paying!',
+                    date: getCurrentDay(10),
+                    time: '11:30',
+                    duration: 90,
+                    bgcolor: 'teal',
+                    icon: 'fas fa-hamburger'
+                },
+                {
+                    id: 5,
+                    title: 'Visit mom',
+                    details: 'Always a nice chat with mom',
+                    date: getCurrentDay(20),
+                    time: '17:00',
+                    duration: 90,
+                    bgcolor: 'grey',
+                    icon: 'fas fa-car'
+                },
+                {
+                    id: 6,
+                    title: 'Conference',
+                    details: 'Teaching Javascript 101',
+                    date: getCurrentDay(27),
+                    time: '08:00',
+                    duration: 540,
+                    bgcolor: 'blue',
+                    icon: 'fas fa-chalkboard-teacher'
+                },
+                {
+                    id: 7,
+                    title: 'Girlfriend',
+                    details: 'Meet GF for dinner at Swanky Restaurant',
+                    date: getCurrentDay(22),
+                    time: '19:00',
+                    duration: 180,
+                    bgcolor: 'teal',
+                    icon: 'fas fa-utensils'
+                },
+                {
+                    id: 8,
+                    title: 'Fishing',
+                    details: 'Time for some weekend R&R',
+                    date: getCurrentDay(27),
+                    bgcolor: 'purple',
+                    icon: 'fas fa-fish',
+                    days: 2
+                },
+                {
+                    id: 9,
+                    title: 'Vacation',
+                    details:
+                        "Trails and hikes, going camping! Don't forget to bring bear spray!",
+                    date: getCurrentDay(29),
+                    bgcolor: 'purple',
+                    icon: 'fas fa-plane',
+                    days: 5
+                }
+            ],
+            isOpen: false
         };
     },
+
+    computed: {
+        // convert the events into a map of lists keyed by date
+        eventsMap() {
+            const map = {};
+            // this.events.forEach(event => (map[ event.date ] = map[ event.date ] || []).push(event))
+            this.events.forEach((event) => {
+                if (!map[event.date]) {
+                    map[event.date] = [];
+                }
+                map[event.date].push(event);
+                if (event.days) {
+                    let timestamp = parseTimestamp(event.date);
+                    let days = event.days;
+                    do {
+                        timestamp = addToDate(timestamp, { day: 1 });
+                        if (!map[timestamp.date]) {
+                            map[timestamp.date] = [];
+                        }
+                        map[timestamp.date].push(event);
+                    } while (--days > 0);
+                }
+            });
+            return map;
+        }
+    },
+
     methods: {
+        badgeClasses(event, type) {
+            const isHeader = type === 'header';
+            return {
+                [`text-white bg-${event.bgcolor}`]: true,
+                'full-width':
+                    !isHeader && (!event.side || event.side === 'full'),
+                'left-side': !isHeader && event.side === 'left',
+                'right-side': !isHeader && event.side === 'right',
+                'rounded-border': true
+            };
+        },
+
+        badgeStyles(
+            event,
+            type,
+            timeStartPos = undefined,
+            timeDurationHeight = undefined
+        ) {
+            const s = {};
+            if (timeStartPos && timeDurationHeight) {
+                s.top = timeStartPos(event.time) + 'px';
+                s.height = timeDurationHeight(event.duration) + 'px';
+            }
+            s['align-items'] = 'flex-start';
+            return s;
+        },
+
+        getEvents(dt) {
+            // get all events for the specified date
+            const events = this.eventsMap[dt] || [];
+
+            if (events.length === 1) {
+                events[0].side = 'full';
+            } else if (events.length === 2) {
+                // this example does no more than 2 events per day
+                // check if the two events overlap and if so, select
+                // left or right side alignment to prevent overlap
+                const startTime = addToDate(parsed(events[0].date), {
+                    minute: parseTime(events[0].time)
+                });
+                const endTime = addToDate(startTime, {
+                    minute: events[0].duration
+                });
+                const startTime2 = addToDate(parsed(events[1].date), {
+                    minute: parseTime(events[1].time)
+                });
+                const endTime2 = addToDate(startTime2, {
+                    minute: events[1].duration
+                });
+                if (
+                    isBetweenDates(startTime2, startTime, endTime, true) ||
+                    isBetweenDates(endTime2, startTime, endTime, true)
+                ) {
+                    events[0].side = 'left';
+                    events[1].side = 'right';
+                } else {
+                    events[0].side = 'full';
+                    events[1].side = 'full';
+                }
+            }
+
+            return events;
+        },
+
+        scrollToEvent(event) {
+            this.$refs.calendar.scrollToTime(event.time, 350);
+        },
+
         onToday() {
             this.$refs.calendar.moveToToday();
         },
@@ -68,6 +363,7 @@ export default defineComponent({
             console.log('onClickDate', data);
         },
         onClickTime(data) {
+            this.isOpen = true;
             console.log('onClickTime', data);
         },
         onClickInterval(data) {
@@ -82,3 +378,60 @@ export default defineComponent({
     }
 });
 </script>
+
+<style lang="sass" scoped>
+.my-event
+  position: absolute
+  font-size: 12px
+  justify-content: center
+  margin: 0 1px
+  text-overflow: ellipsis
+  overflow: hidden
+  cursor: pointer
+
+.title
+  position: relative
+  display: flex
+  justify-content: center
+  align-items: center
+  height: 100%
+
+.text-white
+  color: white
+
+.bg-blue
+  background: blue
+
+.bg-green
+  background: green
+
+.bg-orange
+  background: orange
+
+.bg-red
+  background: red
+
+.bg-teal
+  background: teal
+
+.bg-grey
+  background: grey
+
+.bg-purple
+  background: purple
+
+.full-width
+  left: 0
+  width: calc(100% - 2px)
+
+.left-side
+  left: 0
+  width: calc(50% - 3px)
+
+.right-side
+  left: 50%
+  width: calc(50% - 3px)
+
+.rounded-border
+  border-radius: 2px
+</style>
