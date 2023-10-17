1e960fcfaf3d1af6616e9e9c5b2bbc44bc00f5c1
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-10-17 10:56:36 +0200

    mostrar usarios sin tados

--- src/modules/users/components/user-list/UserList.vue
+++ src/modules/users/components/user-list/UserList.vue
@@ -25,41 +25,6 @@ export interface ColumnTable {
 
 const columnsUser: ColumnTable[] = [
     {
-        name: 'photo',
-        align: 'left',
-        label: t('user.label.photo'),
-        field: 'photo',
-        sortable: false
-    },
-    {
-        name: 'name',
-        align: 'left',
-        label: t('user.label.name'),
-        field: 'name',
-        sortable: true
-    },
-    {
-        name: 'surnames',
-        align: 'left',
-        label: t('user.label.surnames'),
-        field: 'surnames',
-        sortable: true
-    },
-    {
-        name: 'email',
-        align: 'left',
-        label: t('user.label.email'),
-        field: 'email',
-        sortable: true
-    },
-    {
-        name: 'roles',
-        align: 'left',
-        label: t('user.label.permissions'),
-        field: 'roles',
-        sortable: true
-    },
-    {
         name: 'actions',
         align: 'right',
         label: t('user.label.actions'),
@@ -70,5 +35,72 @@ const columnsUser: ColumnTable[] = [
 </script>
 
 <template>
-    <div class="row"></div>
+    <div class="row">
+        <q-table
+            :rows="users"
+            :columns="columnsUser"
+            row-key="id"
+            class="col-12"
+            :loading="loading"
+            :no-data-label="$t('shared.label.noData')"
+            :rows-per-page-label="$t('shared.label.recordsPerPage')"
+        >
+            <template v-slot:top>
+                <span class="text-h6"> {{ $t('user.label.users') }} </span>
+                <q-space />
+                <q-btn
+                    v-if="$q.platform.is.desktop"
+                    :label="$t('user.label.createUser')"
+                    color="primary"
+                    icon="mdi-plus"
+                    dense
+                    @click="
+                        $router.push({
+                            name: 'users-add'
+                        })
+                    "
+                />
+            </template>
+            <template v-slot:body-cell-photo="props">
+                <q-td :props="props">
+                    <q-avatar v-if="props.row.photo">
+                        <q-img :ratio="1" :src="props.row.photo" />
+                    </q-avatar>
+                    <q-avatar
+                        v-else
+                        color="grey"
+                        text-color="white"
+                        icon="mdi-image-off"
+                    />
+                </q-td>
+            </template>
+            <template v-slot:body-cell-actions="props">
+                <q-td :props="props" class="q-gutter-x-sm">
+                    <q-btn
+                        icon="mdi-pencil-outline"
+                        color="info"
+                        dense
+                        size="sm"
+                        @click="
+                            $router.push({
+                                name: 'users-edit',
+                                params: { id: props.row.id }
+                            })
+                        "
+                    >
+                        <q-tooltip> {{ $t('user.label.edit') }} </q-tooltip>
+                    </q-btn>
+                    <q-btn
+                        icon="mdi-delete-outline"
+                        color="negative"
+                        dense
+                        size="sm"
+                        @click="removeUser(props.row.id)"
+                    >
+                        <q-tooltip> {{ $t('user.label.delete') }} </q-tooltip>
+                    </q-btn>
+                </q-td>
+            </template>
+        </q-table>
+    </div>
 </template>
