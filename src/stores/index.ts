e0ed7b70f3af7cafd2192e1d28b21c44d423d3dd
Jaume Parrot Altisent <jparrot.plexus@externos.avatel.es>
2025-03-19 15:03:05 +0100

    first commit

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -15,7 +15,7 @@ declare module 'pinia' {
 
 /*
  * If not building with SSR mode, you can
- * directly export the Store instantiation;
+ * directly export the Store instantiation
  *
  * The function below can be async too; either use
  * async/await or return a Promise which resolves
